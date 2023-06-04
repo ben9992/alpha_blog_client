cb5d7a2994b5d0e206d605af6d258a94660e4418
Ben Mishali <you@example.com>
2023-06-04 19:37:27 +0300

    fix user

--- src/Components/User/UserProfile.jsx
+++ src/Components/User/UserProfile.jsx
@@ -1,55 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
-import { useNavigate, useParams } from "react-router-dom";
-import { IsUserLoggedIn } from "../Auth/Auth";
-import { axios, updateToken } from '../Auth/Axios';
-import { PostsList } from '../Posts/PostsList';
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { axios, updateToken } from "../Auth/Axios";
+import { PostsList } from "../Posts/PostsList";
 
 const UserProfile = () => {
-  const [user, setUser] = useState(null);
-  const { userId } = useParams();
-  const navigate = useNavigate();
-  
-  useEffect(() => {
-    const isUserFound = IsUserLoggedIn()
-    if(!isUserFound)
-      return navigate('/login');
+	const [user, setUser] = useState(null);
+	const { userId } = useParams();
 
-    const token = localStorage.getItem('token');
-    updateToken(token)
-    const getUser = async() => {
-      const user = await axios.get(`/users/${userId}`).then(user => user).catch(err => {
-        return false
-      })
-  
-      if(!user.data)
-          return false
-  
-      setUser(user.data)
-    }
-    getUser()
-  }, [])
+	useEffect(() => {
+		const token = localStorage.getItem("token");
+		updateToken(token);
+		const getUser = async () => {
+			const user = await axios
+				.get(`/users/${userId}`)
+				.then((user) => user)
+				.catch((err) => {
+					return false;
+				});
 
+			if (!user.data) return false;
 
-  return !user ? (<></>) : (
-    <Container>
-      <Row className="row-md-8 justify-content-center">
-        <Col>
-          <Row className="row-md-8 justify-content-center">
-            <Image style={{width: "20%", padding: "1rem"}} src={process.env.REACT_APP_API_SERVER_URL + user.profileImage} roundedCircle />
-          </Row>
-          <Row className="row-md-8 justify-content-center text-center">
-            <h1>{user.username}</h1>
-          </Row>
-        </Col>
-          
-          
-      </Row>
-      <Row className="row-md-8 justify-content-center">
-        <PostsList userId={userId}/>
-      </Row>
-    </Container>
-  );
+			setUser(user.data);
+		};
+		getUser();
+	}, []);
+
+	return !user ? (
+		<></>
+	) : (
+		<Container>
+			<Row className="row-md-8 justify-content-center">
+				<Col>
+					<Row className="row-md-8 justify-content-center">
+						<Image
+							style={{ width: "20%", padding: "1rem" }}
+							src={process.env.REACT_APP_API_SERVER_URL + user.profileImage}
+							roundedCircle
+						/>
+					</Row>
+					<Row className="row-md-8 justify-content-center text-center">
+						<h1>{user.username}</h1>
+					</Row>
+				</Col>
+			</Row>
+			<Row className="row-md-8 justify-content-center">
+				<PostsList userId={userId} />
+			</Row>
+		</Container>
+	);
 };
 
 export default UserProfile;
